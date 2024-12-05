358211ee9813e4bfa2f4bf86bcdce90b93618d5d
Kalvin Kayi <161039123+kayikalvin@users.noreply.github.com>
2024-12-05 12:39:31 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ st.title('🎈 Streamlit_ml')
 st.info('This app creates a machine learning model')
 
 st.write('Hello world!')
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+df
